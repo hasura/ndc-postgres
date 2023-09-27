c84f18501323163401dc86ec381099c699d470fa
Daniel Harvey <danieljamesharvey@gmail.com>
2023-09-27 16:56:06 +0100

    Pull metrics in CI benchmarks (#319)
    <!-- The PR description should answer 2 (maybe 3) important questions:
    -->
    
    ### What
    
    Add new values to our benchmarks. The top two already existed.
    
    `connection acquisition time` - time waiting for a connection from the
    connection pool.
    
    `request time - (query + acquisition)` is the full request time minus
    connection acquisition and query execution time.
    
    `processing time` is the same thing, but the "full request" is only time
    within `postgres-ndc`.
    
    ```json
    [
      {
        "name": "select-by-pk - median",
        "unit": "ms",
        "value": 161.8641895
      },
      {
        "name": "select-by-pk - p(95)",
        "unit": "ms",
        "value": 330.5366615
      },
      {
        "name": "select-by-pk - connection acquisition time",
        "unit": "ms",
        "value": 85.30638819888003
      },
      {
        "name": "select-by-pk - request time - (query + acquisition)",
        "unit": "ms",
        "value": 77.98981615823729
      },
      {
        "name": "select-by-pk - processing time",
        "unit": "ms",
        "value": 1.1815740711703955
      }
    ]
    ```
    
    ### How
    
    Query prometheus metrics after each run and combine these with K6 stats.

--- benchmarks/component/summarize.sh
+++ benchmarks/component/summarize.sh
@@ -20,6 +20,8 @@ rm -f "$directory"
 mkdir "$directory"
 trap 'rm -rf "$directory"' EXIT
 
+# the output of the K6 jobs is sitting in the `benchmark_output` volume,
+# so we mount it in an arbitrary container which we use to copy it out.
 docker run \
   --rm \
   --mount 'type=volume,source=ndc-postgres-component-benchmarks_benchmark_output,target=/output' \
@@ -27,12 +29,31 @@ docker run \
   tar c -C /output . \
   | tar x -C "$directory"
 
+# the output of the metrics for each job is sitting in the `metrics_output` volume,
+# so we mount it in an arbitrary container which we use to copy it out.
+docker run \
+  --rm \
+  --mount 'type=volume,source=postgres-ndc-component-benchmarks_metrics_output,target=/output' \
+  busybox \
+  tar c -C /output . \
+  | tar x -C "$directory"
+
 for summary_file in "$directory/summaries/"*; do
   name="$(basename "$summary_file" | sed 's/__.*//')"
+  metrics_file="${directory}/metrics-${name}.js.json"
+  query_time=$(jq -c '.query_execution' "$metrics_file")
+  connection_time=$(jq -c '.connection_acquisition' "$metrics_file")
+  processing_time=$(jq -c '.processing' "$metrics_file")
   jq -c --arg name "$name" \
-    '.metrics.http_req_duration.values | [
-      {"name": ($name + " - median"), "unit": "ms", "value": .med},
-      {"name": ($name + " - p(95)"), "unit": "ms", "value": ."p(95)"}
+        --arg query_time "$query_time" \
+        --arg connection_time "$connection_time" \
+        --arg processing_time "$processing_time" \
+    '.metrics | [
+      {"name": ($name + " - median"), "unit": "ms", "value": .http_req_duration.values.med},
+      {"name": ($name + " - p(95)"), "unit": "ms", "value": .http_req_duration.values."p(95)"},
+      {"name": ($name + " - connection acquisition time"), "unit": "ms", "value": (($connection_time | fromjson | tonumber) / .http_reqs.values.count ) },
+      {"name": ($name + " - request time - (query + acquisition)"), "unit": "ms", "value": (.http_req_duration.values.avg - (($query_time | fromjson | tonumber) / .http_reqs.values.count) - (($connection_time | fromjson | tonumber) / .http_reqs.values.count)) },
+      {"name": ($name + " - processing time"), "unit": "ms", "value": (($processing_time | fromjson | tonumber) / .http_reqs.values.count ) }
     ]' \
     "$summary_file"
 done | jq -s 'flatten(1)'
