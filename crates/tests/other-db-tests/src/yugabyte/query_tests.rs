eff5977fabb9d4645df1d2b2467f37e2c703d53a
Samir Talwar <samir.talwar@hasura.io>
2023-10-05 13:33:54 +0200

    Re-add `select_value_types` for Yugabyte.

--- crates/tests/other-db-tests/src/yugabyte/query_tests.rs
+++ crates/tests/other-db-tests/src/yugabyte/query_tests.rs
@@ -385,3 +385,15 @@ mod native_queries {
         insta::assert_json_snapshot!(result);
     }
 }
+
+#[cfg(test)]
+mod types {
+    use super::super::common::create_router;
+    use tests_common::request::run_query;
+
+    #[tokio::test]
+    async fn select_value_types() {
+        let result = run_query(create_router().await, "value_types").await;
+        insta::assert_json_snapshot!(result);
+    }
+}
