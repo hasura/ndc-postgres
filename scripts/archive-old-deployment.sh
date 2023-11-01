50ff04054b0dbe0f7e5dcea8a1d43ba506da2142
Daniel Harvey <danieljamesharvey@gmail.com>
2023-11-01 16:24:24 +0000

    Test old deployment versions

--- scripts/archive-old-deployment.sh
+++ scripts/archive-old-deployment.sh
@@ -11,12 +11,11 @@ CURRENT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" > /dev/null && echo "$P
 
 SNAPSHOT_DIR="$(realpath ${CURRENT_DIR}/../static/deployment-snapshots)"
 
+# create snapshot dir if does not exist
 mkdir -p "$SNAPSHOT_DIR"
 
+# create filename from hash of contents
 NEW_FILENAME="$(sha256sum "${CHINOOK_DEPLOYMENT}" | cut -f1 -d' ').json"
 
-echo "$SNAPSHOT_DIR"
-
-echo "${CHINOOK_DEPLOYMENT}"
-
+# copy current deployment to new filename
 cp "${CHINOOK_DEPLOYMENT}" "${SNAPSHOT_DIR}/${NEW_FILENAME}"
