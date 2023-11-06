927c903f564b64dbb4ef35e5c22a915234a4cdc3
Samir Talwar <samir.talwar@hasura.io>
2023-11-06 17:36:07 +0100

    Fix Nix cross-compilation by using `callPackage` properly. (#151)
    ### What
    
    I removed the call to `callPackage` in favor of using `pkgs` directly.
    It seems this was a bad idea. I have undone this part of my refactor
    and, I hope, made the Nix derivation clearer as a result.
    
    I also added CI jobs for cross-compilation so this doesn't happen again.
    
    ### How
    
    Yet more Nix. The key is using `pkgs.callPackage` with the `pkgs` from
    _rust.nix_, rather than `import`, to specify the derivation.

--- nix/app.nix
+++ nix/app.nix
@@ -1,8 +1,14 @@
 # This is a function that returns a derivation for the compiled Rust project.
-{ rust }:
+{ craneLib
+, lib
+, stdenv
+, openssl
+, libiconv
+, pkg-config
+, protobuf
+, darwin
+}:
 let
-  inherit (rust) pkgs;
-
   buildArgs = {
     pname = "ndc-postgres";
 
@@ -13,36 +19,31 @@ let
         isSourceFile = path: type:
           isJsonFile path type
           || isSqlFile path type
-          || rust.craneLib.filterCargoSources path type;
+          || craneLib.filterCargoSources path type;
       in
-      pkgs.lib.cleanSourceWith { src = rust.craneLib.path ./..; filter = isSourceFile; };
+      lib.cleanSourceWith { src = craneLib.path ./..; filter = isSourceFile; };
 
     buildInputs = [
-      pkgs.openssl
-    ] ++ pkgs.lib.optionals pkgs.stdenv.hostPlatform.isDarwin [
-      pkgs.libiconv
-      pkgs.darwin.apple_sdk.frameworks.Security
-      pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
+      openssl
+    ] ++ lib.optionals stdenv.hostPlatform.isDarwin [
+      libiconv
+      darwin.apple_sdk.frameworks.Security
+      darwin.apple_sdk.frameworks.SystemConfiguration
     ];
 
     nativeBuildInputs = [
-      pkgs.pkg-config # required for non-static builds
-      pkgs.protobuf # required by opentelemetry-proto, a dependency of axum-tracing-opentelemetry
+      pkg-config # required for non-static builds
+      protobuf # required by opentelemetry-proto, a dependency of axum-tracing-opentelemetry
     ];
   };
 
   # Build the dependencies first.
-  cargoArtifacts = rust.craneLib.buildDepsOnly buildArgs;
-
-  # Then build the crate.
-  crate = rust.craneLib.buildPackage
-    (buildArgs // {
-      inherit cargoArtifacts;
-      doCheck = false;
-    });
+  cargoArtifacts = craneLib.buildDepsOnly buildArgs;
 in
-# Override the derivation to add cross-compilation and static linking environment variables.
-crate.overrideAttrs (previous: rust.buildEnv // {
-  # We also have to override the `cargoArtifacts` derivation with the same changes.
-  cargoArtifacts = previous.cargoArtifacts.overrideAttrs (previous: rust.buildEnv);
-})
+# Then build the crate.
+craneLib.buildPackage
+  (buildArgs // {
+    inherit cargoArtifacts;
+    doCheck = false;
+    cargoExtraArgs = "--locked --bin ${buildArgs.pname}";
+  })
