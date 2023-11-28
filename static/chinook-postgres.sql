211e7298fe6f57bd5653207f7a54a3e73f37ac6c
Gil Mizrahi <gil@hasura.io>
2023-11-28 14:22:48 +0200

    copy

--- static/chinook-postgres.sql
+++ static/chinook-postgres.sql
@@ -15828,10 +15828,3 @@ INSERT INTO "PlaylistTrack" ("PlaylistId", "TrackId") VALUES (17, 2095);
 INSERT INTO "PlaylistTrack" ("PlaylistId", "TrackId") VALUES (17, 2096);
 INSERT INTO "PlaylistTrack" ("PlaylistId", "TrackId") VALUES (17, 3290);
 INSERT INTO "PlaylistTrack" ("PlaylistId", "TrackId") VALUES (18, 597);
-
--- create a template from the database so we can quickly copy it when
--- we test mutations.
-SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity
-WHERE pg_stat_activity.datname = 'postgres' AND pid <> pg_backend_pid();
-
-CREATE DATABASE chinook_template WITH TEMPLATE postgres;
