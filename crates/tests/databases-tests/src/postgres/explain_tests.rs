2612203a33ccd40009222a9137cfb6676706d843
Gil Mizrahi <gil@hasura.io>
2023-12-13 14:44:37 +0200

    support variables inside native queries

--- crates/tests/databases-tests/src/postgres/explain_tests.rs
+++ crates/tests/databases-tests/src/postgres/explain_tests.rs
@@ -31,4 +31,18 @@ mod explain {
         is_contained_in_lines(keywords, result.details.plan);
         insta::assert_snapshot!(result.details.query);
     }
+
+    mod native_queries {
+        use super::super::super::common::create_router;
+        use tests_common::assert::is_contained_in_lines;
+        use tests_common::request::run_explain;
+
+        #[tokio::test]
+        async fn embedded_variable() {
+            let result =
+                run_explain(create_router().await, "native_queries/embedded_variable").await;
+            is_contained_in_lines(vec!["Aggregate", "Seq Scan", "Filter"], result.details.plan);
+            insta::assert_snapshot!(result.details.query);
+        }
+    }
 }
