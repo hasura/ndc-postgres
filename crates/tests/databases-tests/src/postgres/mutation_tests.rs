a8f16b7f08797781c7cfa58c8e2ea070346d5e34
Gil Mizrahi <gil@hasura.io>
2024-01-02 16:40:59 +0200

    add a negative test for mutations rollback

--- crates/tests/databases-tests/src/postgres/mutation_tests.rs
+++ crates/tests/databases-tests/src/postgres/mutation_tests.rs
@@ -3,7 +3,7 @@
 mod basic {
     use super::super::common;
     use tests_common::deployment::{clean_up_deployment, create_fresh_deployment};
-    use tests_common::request::run_mutation;
+    use tests_common::request::{run_mutation, run_query};
 
     #[tokio::test]
     async fn delete_playlist() {
@@ -33,11 +33,15 @@ mod basic {
         .await
         .unwrap();
 
-        let result = run_mutation(
-            tests_common::router::create_router_from_deployment(&deployment.deployment_path).await,
-            "insert_artist_album",
-        )
-        .await;
+        let router =
+            tests_common::router::create_router_from_deployment(&deployment.deployment_path).await;
+
+        let mutation_result = run_mutation(router.clone(), "insert_artist_album").await;
+
+        // expect the artist to be returned, because we inserted it successfully.
+        let selection_result = run_query(router, "mutations/select_specific_artist").await;
+
+        let result = (mutation_result, selection_result);
 
         clean_up_deployment(deployment).await.unwrap();
         insta::assert_json_snapshot!(result)
@@ -60,5 +64,37 @@ mod basic {
 
         clean_up_deployment(deployment).await.unwrap();
         insta::assert_json_snapshot!(result)
+    }
+}
+
+#[cfg(test)]
+mod negative {
+    use super::super::common;
+    use tests_common::deployment::{clean_up_deployment, create_fresh_deployment};
+    use tests_common::request::{run_mutation500, run_query};
+
+    #[tokio::test]
+    /// Check that the second statement fails on duplicate key constraint,
+    /// and that it rolls back the first statement.
+    async fn insert_artist_album_bad() {
+        let deployment = create_fresh_deployment(
+            common::CONNECTION_STRING,
+            common::CHINOOK_DEPLOYMENT_PATH_V2,
+        )
+        .await
+        .unwrap();
+
+        let router =
+            tests_common::router::create_router_from_deployment(&deployment.deployment_path).await;
+
+        let mutation_result = run_mutation500(router.clone(), "insert_artist_album_bad").await;
+
+        // expect no rows returned because first operation was rolled back.
+        let selection_result = run_query(router, "mutations/select_specific_artist").await;
+
+        let result = (mutation_result, selection_result);
+
+        clean_up_deployment(deployment).await.unwrap();
+        insta::assert_json_snapshot!(result);
     }
 }
