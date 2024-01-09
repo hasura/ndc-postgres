626f590950956486d95326f97e63ad6b65e56dd4
Gil Mizrahi <gil@hasura.io>
2024-01-09 12:21:06 +0200

    move openapi test

--- crates/documentation/openapi/src/generator.rs
+++ crates/documentation/openapi/src/generator.rs
@@ -6,16 +6,3 @@ pub fn generate_schema() -> RootSchema {
         .into_generator()
         .into_root_schema_for::<RawConfiguration>()
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn up_to_date_generated_schema() {
-        let generated_schema = generate_schema();
-        let generated_schema_json = serde_json::to_value(&generated_schema).unwrap();
-
-        insta::assert_json_snapshot!(generated_schema_json);
-    }
-}
