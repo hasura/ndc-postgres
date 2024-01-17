36d1944097512ffad0b2df3e14ecb19e6f6c40c1
Philip Lykke Carlsen <plcplc@gmail.com>
2024-01-17 13:41:07 +0100

    Check that the database is actually empty

--- scripts/reinitialize-aurora.sh
+++ scripts/reinitialize-aurora.sh
@@ -6,8 +6,24 @@ set -e -u -o pipefail
 # .envrc.local) is assumed to hold the authenticated connection string to the
 # writable aurora instance to act on.
 
-# In order to avoid accidental loss of data the database is assumed to already
-# be suitably empty.
+# In order to avoid accidental loss of data the database must be suitably empty before proceeding.
+
+number_of_existing_relations=$(
+  psql $AURORA_CONNECTION_STRING -t <<QUERY
+SELECT
+  COUNT(*)
+FROM pg_class
+WHERE
+  relnamespace = 'public'::regnamespace
+QUERY
+)
+
+if [ "$number_of_existing_relations" != 0 ];
+then
+  echo There appears to be $number_of_existing_relations relations still left in the "public" namespace.
+  echo Bailing out.
+  exit 1
+fi
 
 psql "$AURORA_CONNECTION_STRING" <<DOC
 
