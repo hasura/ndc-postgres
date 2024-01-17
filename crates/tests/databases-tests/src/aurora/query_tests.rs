3765c1a77e2bcb9ddc4800dc75fdc6a2dd4a0c9e
Philip Lykke Carlsen <plcplc@gmail.com>
2024-01-17 16:04:48 +0100

    Add tests of composite types to Aurora (#259)
    ### What
    
    This PR extends the tests of composite types to cover Aurora.
    
    This is a follow-up to #257, and will be rebased once that is merged.
    
    ### How
    
    In order to standardise the (rare) management we need to be able to do
    on our Aurora test instance this PR adds a script that inserts all the
    test data currently in use. Currently, this means: The chinook dataset,
    and definitions of composite types.
    
    ---------
    
    Co-authored-by: Samir Talwar <samir.talwar@hasura.io>

--- crates/tests/databases-tests/src/aurora/query_tests.rs
+++ crates/tests/databases-tests/src/aurora/query_tests.rs
@@ -40,28 +40,24 @@ mod basic {
     }
 
     #[tokio::test]
-    #[ignore = "Pending update of aurora test database"]
     async fn select_composite_column_simple() {
         let result = run_query(create_router().await, "select_composite_column_simple").await;
         insta::assert_json_snapshot!(result);
     }
 
     #[tokio::test]
-    #[ignore = "Pending update of aurora test database"]
     async fn select_composite_column_complex() {
         let result = run_query(create_router().await, "select_composite_column_complex").await;
         insta::assert_json_snapshot!(result);
     }
 
     #[tokio::test]
-    #[ignore = "Pending update of aurora test database"]
     async fn select_composite_variable_simple() {
         let result = run_query(create_router().await, "select_composite_variable_simple").await;
         insta::assert_json_snapshot!(result);
     }
 
     #[tokio::test]
-    #[ignore = "Pending update of aurora test database"]
     async fn select_composite_variable_complex() {
         let result = run_query(create_router().await, "select_composite_variable_complex").await;
         insta::assert_json_snapshot!(result);
