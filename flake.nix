b164b7aa544b53b666496e46ad66730a6629312f
Samir Talwar <samir.talwar@hasura.io>
2024-02-12 16:26:48 +0100

    Nix: Fix the build inputs for the shell and app. (#285)
    This makes a few changes to the Nix flake to get things building on my
    machine.
    
    1. On macOS, add `CoreFoundation` to the list of dependencies.
    2. Adds `strictDeps = true` to the package, which means we need to add
    `openssl.dev` (and `pkg-config`) to `nativeBuildInputs` as they're
    required at build-time.
    3. Move `openssl` and `protobuf` to `buildInputs` as they're required at
    runtime.
    4. Reuse inputs from the default package in the shell.
    5. Hide the `perf` shell from macOS so `nix flake check` works.
    
    The last point is partially undoing a change I made earlier, where the
    shell brought in inputs from all flake checks. This was way too broad;
    this narrows it a lot.

--- flake.nix
+++ flake.nix
@@ -82,53 +82,51 @@
 
         formatter = pkgs.nixpkgs-fmt;
 
-        devShells.default = pkgs.mkShell {
-          # build-time inputs
-          nativeBuildInputs = [
-            # Development
-            pkgs.just
-            pkgs.nixpkgs-fmt
-            pkgs.nodePackages.prettier
-            pkgs.pkg-config
+        devShells = {
+          default = pkgs.mkShell {
+            # include dependencies of the default package
+            inputsFrom = [ self.packages.${localSystem}.default ];
 
-            # Rust
-            pkgs.cargo-edit
-            pkgs.cargo-expand
-            pkgs.cargo-flamegraph
-            pkgs.cargo-insta
-            pkgs.cargo-machete
-            pkgs.cargo-nextest
-            pkgs.cargo-watch
-            pkgs.rnix-lsp
-            rust.rustToolchain
+            # build-time inputs
+            nativeBuildInputs = [
+              # Development
+              pkgs.just
+              pkgs.nixpkgs-fmt
+              pkgs.nodePackages.prettier
 
-            # Benchmarks
-            pkgs.k6
+              # Rust
+              pkgs.cargo-edit
+              pkgs.cargo-expand
+              pkgs.cargo-flamegraph
+              pkgs.cargo-insta
+              pkgs.cargo-machete
+              pkgs.cargo-nextest
+              pkgs.cargo-watch
+              pkgs.rnix-lsp
+              rust.rustToolchain
 
-            # Deployment
-            pkgs.skopeo
-          ];
+              # Benchmarks
+              pkgs.k6
 
-          # runtime inputs
-          buildInputs = [
-            pkgs.openssl
-            pkgs.protobuf
-          ];
-        };
+              # Deployment
+              pkgs.skopeo
+            ];
+          };
+        } // pkgs.lib.attrsets.optionalAttrs pkgs.hostPlatform.isLinux {
+          # This performance-testing shell will only work on Linux.
+          perf = pkgs.mkShell {
+            inputsFrom = [
+              self.devShells.${localSystem}.default
+            ];
 
-        # This performance-testing shell will only work on Linux.
-        devShells.perf = pkgs.mkShell {
-          inputsFrom = [
-            self.devShells.${localSystem}.default
-          ];
-
-          # build-time inputs
-          nativeBuildInputs = [
-            pkgs.heaptrack
-            pkgs.linuxPackages_latest.perf
-            pkgs.mold-wrapped
-            pkgs.valgrind
-          ];
+            # build-time inputs
+            nativeBuildInputs = [
+              pkgs.heaptrack
+              pkgs.linuxPackages_latest.perf
+              pkgs.mold-wrapped
+              pkgs.valgrind
+            ];
+          };
         };
       }
     );
