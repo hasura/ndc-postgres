59415d44722101d81292cd9cabadb22e03083535
Samir Talwar <samir.talwar@hasura.io>
2024-02-13 10:44:57 +0100

    Update crates/connectors/ndc-postgres/src/configuration_mapping.rs
    Co-authored-by: Gil Mizrahi <gil@hasura.io>

--- crates/connectors/ndc-postgres/src/configuration_mapping.rs
+++ crates/connectors/ndc-postgres/src/configuration_mapping.rs
@@ -1,3 +1,8 @@
+//! A module for converting ndc-configuration data type into query-engine data types.
+
+/// Convert a user-specified configuration of the isolation level for transactions
+/// into a SQL data type representing that isolation level, which will be passed
+/// to the engine on requests.
 pub(crate) fn convert_isolation_level(
     input: ndc_postgres_configuration::IsolationLevel,
 ) -> query_engine_sql::sql::ast::transaction::IsolationLevel {
