269f6dc26e40665a9e8696c800227613f6f0934c
Samir Talwar <samir.talwar@hasura.io>
2024-02-21 13:48:06 +0100

    Add commentary to the CLI files.

--- crates/cli/src/lib.rs
+++ crates/cli/src/lib.rs
@@ -1,3 +1,8 @@
+//! The interpretation of the commands that the CLI cah handle.
+//!
+//! The CLI can do a few things. This provides a central point where those things are routed and
+//! then done, making it easier to test this crate deterministically.
+
 use std::fs;
 use std::path::Path;
 
@@ -5,24 +10,31 @@ use clap::{Parser, Subcommand};
 
 use ndc_postgres_configuration as configuration;
 
+/// The command-line arguments.
+///
+/// By rights this should be in main.rs, but `clap`'s derivations would leak into this file anyway,
+/// so it seems reasonable to leave this here too.
 #[derive(Debug, Parser)]
 pub struct Args {
     #[command(subcommand)]
     pub subcommand: Command,
 }
 
+/// The command invoked by the user.
 #[derive(Debug, Clone, Subcommand)]
 pub enum Command {
     /// Initialize a configuration in the current (empty) directory.
     Initialize,
 }
 
+/// The set of errors that can go wrong _in addition to_ generic I/O or parsing errors.
 #[derive(Debug, PartialEq, thiserror::Error)]
 pub enum Error {
     #[error("directory is not empty")]
     DirectoryIsNotEmpty,
 }
 
+/// Run a command in a given directory.
 pub fn run(command: Command, configuration_dir: &Path) -> anyhow::Result<()> {
     match command {
         Command::Initialize => initialize(configuration_dir)?,
