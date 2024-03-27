9accf8fdf314f9a6c60153294094f8e520a0b2a8
Philip Lykke Carlsen <plcplc@gmail.com>
2024-03-27 14:06:16 +0100

    test

--- static/composite-types-simple.sql
+++ static/composite-types-simple.sql
@@ -13,3 +13,15 @@ CREATE TYPE person_address AS
     address_line_1 text,
     address_line_2 text
   );
+
+-- The below types and tables serve to show that composite types inform which scalar types are relevant.
+
+CREATE TYPE discoverable_types AS
+  (
+    only_occurring_here1 bit
+  );
+
+CREATE TABLE discoverable_types_root_occurrence
+  (
+    col discoverable_types
+  );
