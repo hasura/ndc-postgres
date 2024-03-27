5125d3bcebef1b6eff177c26cfb159cd40bc36b9
Samir Talwar <samir.talwar@hasura.io>
2024-03-27 10:17:22 +0100

    Disable ndc-tests for old versions of PostgreSQL.

--- crates/tests/databases-tests/src/postgres/ndc_tests.rs
+++ crates/tests/databases-tests/src/postgres/ndc_tests.rs
@@ -6,7 +6,7 @@ use super::common;
 use tests_common::common_tests::ndc_tests;
 
 #[tokio::test]
-async fn test_connector() -> ndc_tests::Result {
+async fn postgres_current_only_test_connector() -> ndc_tests::Result {
     let router = common::create_router().await;
     ndc_tests::test_connector(router).await
 }
