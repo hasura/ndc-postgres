ac0b94e4a9dab8b828a5e873478886ec1d1b8c53
Philip Lykke Carlsen <plcplc@gmail.com>
2024-04-11 11:17:46 +0200

    Machete and straggler use of serde and schemars

--- crates/query-engine/metadata/src/metadata/mutations.rs
+++ crates/query-engine/metadata/src/metadata/mutations.rs
@@ -1,11 +1,7 @@
 //! Generated mutations-related metadata information.
 
-use schemars::JsonSchema;
-use serde::{Deserialize, Serialize};
-
 /// Which version of the generated mutations will be included in the schema
-#[derive(Debug, PartialEq, Eq, Clone, Copy, Deserialize, Serialize, JsonSchema)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum MutationsVersion {
     V1,
     VeryExperimentalWip,
