8b86ada0660e1b0e27d412a45fae05e2503eb0b3
Philip Lykke Carlsen <plcplc@gmail.com>
2024-04-11 08:09:56 +0200

    Strip business types of exchange-related behavior

--- crates/query-engine/metadata/src/metadata/database.rs
+++ crates/query-engine/metadata/src/metadata/database.rs
@@ -1,22 +1,20 @@
 //! Metadata information regarding the database and tracked information.
 
-use schemars::JsonSchema;
-use serde::{Deserialize, Serialize};
 use std::collections::{BTreeMap, BTreeSet};
 
 /// Map of all known composite types.
-#[derive(Debug, Clone, PartialEq, Eq, Default, Serialize, Deserialize, JsonSchema)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq, Eq, Default)]
+
 pub struct CompositeTypes(pub BTreeMap<String, CompositeType>);
 
 /// A Scalar Type.
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize, JsonSchema)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+
 pub struct ScalarType(pub String);
 
 /// The type of values that a column, field, or argument may take.
-#[derive(Debug, Clone, PartialEq, Eq, Deserialize, Serialize, JsonSchema)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq, Eq)]
+
 pub enum Type {
     ScalarType(ScalarType),
     CompositeType(String),
@@ -25,82 +23,74 @@ pub enum Type {
 
 /// Information about a composite type. These are very similar to tables, but with the crucial
 /// difference that composite types do not support constraints (such as NOT NULL).
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize, JsonSchema)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq, Eq)]
+
 pub struct CompositeType {
     pub name: String,
     pub fields: BTreeMap<String, FieldInfo>,
-    #[serde(default)]
+
     pub description: Option<String>,
 }
 
 /// Information about a composite type field.
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize, JsonSchema)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq, Eq)]
+
 pub struct FieldInfo {
     pub name: String,
     pub r#type: Type,
-    #[serde(default)]
+
     pub description: Option<String>,
 }
 
 /// The complete list of supported binary operators for scalar types.
 /// Not all of these are supported for every type.
-#[derive(Debug, Clone, PartialEq, Eq, Default, Serialize, Deserialize, JsonSchema)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq, Eq, Default)]
+
 pub struct ComparisonOperators(pub BTreeMap<ScalarType, BTreeMap<String, ComparisonOperator>>);
 
 /// Represents a postgres binary comparison operator
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize, JsonSchema)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq, Eq)]
+
 pub struct ComparisonOperator {
     pub operator_name: String,
     pub operator_kind: OperatorKind,
     pub argument_type: ScalarType,
 
-    #[serde(default = "default_true")]
     pub is_infix: bool,
 }
 
 /// Is it a built-in operator, or a custom operator.
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize, JsonSchema)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq, Eq)]
+
 pub enum OperatorKind {
     Equal,
     In,
     Custom,
 }
 
-/// This is quite unfortunate: https://github.com/serde-rs/serde/issues/368
-/// TL;DR: we can't set default literals for serde, so if we want 'is_infix' to
-/// default to 'true', we have to set its default as a function that returns 'true'.
-fn default_true() -> bool {
-    true
-}
-
 /// Mapping from a "table" name to its information.
-#[derive(Debug, Clone, PartialEq, Eq, Default, Serialize, Deserialize, JsonSchema)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq, Eq, Default)]
+
 pub struct TablesInfo(pub BTreeMap<String, TableInfo>);
 
 /// Information about a database table (or any other kind of relation).
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize, JsonSchema)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq, Eq)]
+
 pub struct TableInfo {
     pub schema_name: String,
     pub table_name: String,
     pub columns: BTreeMap<String, ColumnInfo>,
-    #[serde(default)]
+
     pub uniqueness_constraints: UniquenessConstraints,
-    #[serde(default)]
+
     pub foreign_relations: ForeignRelations,
-    #[serde(default)]
+
     pub description: Option<String>,
 }
 
 /// Can this column contain null values
-#[derive(Debug, Clone, PartialEq, Eq, Default, Serialize, Deserialize, JsonSchema)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq, Eq, Default)]
+
 pub enum Nullable {
     #[default]
     Nullable,
@@ -108,8 +98,8 @@ pub enum Nullable {
 }
 
 /// Does this column have a default value.
-#[derive(Debug, Clone, PartialEq, Eq, Default, Serialize, Deserialize, JsonSchema)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq, Eq, Default)]
+
 pub enum HasDefault {
     #[default]
     NoDefault,
@@ -117,8 +107,8 @@ pub enum HasDefault {
 }
 
 /// Is this column an identity column.
-#[derive(Debug, Clone, PartialEq, Eq, Default, Serialize, Deserialize, JsonSchema)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq, Eq, Default)]
+
 pub enum IsIdentity {
     #[default]
     NotIdentity,
@@ -127,8 +117,8 @@ pub enum IsIdentity {
 }
 
 /// Is this column a generated column.
-#[derive(Debug, Clone, PartialEq, Eq, Default, Serialize, Deserialize, JsonSchema)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq, Eq, Default)]
+
 pub enum IsGenerated {
     #[default]
     NotGenerated,
@@ -136,82 +126,66 @@ pub enum IsGenerated {
 }
 
 /// Information about a database column.
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize, JsonSchema)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq, Eq)]
+
 pub struct ColumnInfo {
     pub name: String,
     pub r#type: Type,
-    #[serde(default)]
+
     pub nullable: Nullable,
-    #[serde(skip_serializing_if = "does_not_have_default")]
-    #[serde(default)]
+
     pub has_default: HasDefault,
-    #[serde(skip_serializing_if = "is_not_identity")]
-    #[serde(default)]
+
     pub is_identity: IsIdentity,
-    #[serde(skip_serializing_if = "is_not_generated")]
-    #[serde(default)]
+
     pub is_generated: IsGenerated,
-    #[serde(default)]
+
     pub description: Option<String>,
 }
 
-fn does_not_have_default(has_default: &HasDefault) -> bool {
-    matches!(has_default, HasDefault::NoDefault)
-}
-
-fn is_not_identity(is_identity: &IsIdentity) -> bool {
-    matches!(is_identity, IsIdentity::NotIdentity)
-}
-
-fn is_not_generated(is_generated: &IsGenerated) -> bool {
-    matches!(is_generated, IsGenerated::NotGenerated)
-}
-
 /// A mapping from the name of a unique constraint to its value.
-#[derive(Debug, Clone, PartialEq, Eq, Default, Serialize, Deserialize, JsonSchema)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq, Eq, Default)]
+
 pub struct UniquenessConstraints(pub BTreeMap<String, UniquenessConstraint>);
 
 /// The set of columns that make up a uniqueness constraint.
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize, JsonSchema)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq, Eq)]
+
 pub struct UniquenessConstraint(pub BTreeSet<String>);
 
 /// A mapping from the name of a foreign key constraint to its value.
-#[derive(Debug, Clone, PartialEq, Eq, Default, Serialize, Deserialize, JsonSchema)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq, Eq, Default)]
+
 pub struct ForeignRelations(pub BTreeMap<String, ForeignRelation>);
 
 /// A foreign key constraint.
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize, JsonSchema)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq, Eq)]
+
 pub struct ForeignRelation {
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub foreign_schema: Option<String>,
     pub foreign_table: String,
     pub column_mapping: BTreeMap<String, String>,
 }
 
 /// All supported aggregate functions, grouped by type.
-#[derive(Debug, Clone, PartialEq, Eq, Default, Serialize, Deserialize, JsonSchema)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq, Eq, Default)]
+
 pub struct AggregateFunctions(pub BTreeMap<ScalarType, BTreeMap<String, AggregateFunction>>);
 
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize, JsonSchema)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq, Eq)]
+
 pub struct AggregateFunction {
     pub return_type: ScalarType,
 }
 
 /// Type representation of scalar types, grouped by type.
-#[derive(Debug, Clone, PartialEq, Eq, Default, Serialize, Deserialize, JsonSchema)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq, Eq, Default)]
+
 pub struct TypeRepresentations(pub BTreeMap<ScalarType, TypeRepresentation>);
 
 /// Type representation of a scalar type.
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize, JsonSchema)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq, Eq)]
+
 pub enum TypeRepresentation {
     /// JSON booleans
     Boolean,
@@ -253,36 +227,4 @@ pub enum TypeRepresentation {
     Json,
     /// One of the specified string values
     Enum(Vec<String>),
-}
-
-// tests
-
-#[cfg(test)]
-mod tests {
-    use super::{ScalarType, TypeRepresentation, TypeRepresentations};
-
-    #[test]
-    fn parse_type_representations() {
-        assert_eq!(
-            serde_json::from_str::<TypeRepresentations>(
-                r#"{"int4": "integer", "card_suit": {"enum": ["hearts", "clubs", "diamonds", "spades"]}}"#
-            )
-            .unwrap(),
-            TypeRepresentations(
-                [(
-                    ScalarType("int4".to_string()),
-                    TypeRepresentation::Integer
-                ), (
-                    ScalarType("card_suit".to_string()),
-                    TypeRepresentation::Enum(vec![
-                        "hearts".into(),
-                        "clubs".into(),
-                        "diamonds".into(),
-                        "spades".into()
-                    ])
-                )]
-                .into()
-            )
-        );
-    }
 }
