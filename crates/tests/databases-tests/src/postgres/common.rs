f95ca7973399ccb2eb7282e791417a0a166db8c0
Philip Lykke Carlsen <plcplc@gmail.com>
2024-04-18 13:27:48 +0200

    Update broken metadata, and test that it is kept up to date

--- crates/tests/databases-tests/src/postgres/common.rs
+++ crates/tests/databases-tests/src/postgres/common.rs
@@ -6,6 +6,8 @@ pub const BROKEN_QUERIES_NDC_METADATA_PATH: &str = "static/postgres/broken-queri
 
 pub const CONNECTION_URI: &str = "postgresql://postgres:password@localhost:64002";
 
+pub const EMPTY_CONNECTION_URI: &str = "postgresql://postgres:password@localhost:64002/empty";
+
 /// Creates a router with a fresh state from the test ndc_metadata.
 pub async fn create_router() -> axum::Router {
     tests_common::router::create_router(CHINOOK_NDC_METADATA_PATH, CONNECTION_URI).await
