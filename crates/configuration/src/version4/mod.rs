a2467c57b71cbf2812f784ae034ede657ad8ab34
Philip Lykke Carlsen <plcplc@gmail.com>
2024-05-09 15:33:40 +0200

    Re-introduce 'in' operator and filter occurring types

--- crates/configuration/src/version4/mod.rs
+++ crates/configuration/src/version4/mod.rs
@@ -8,7 +8,7 @@ mod to_runtime_configuration;
 mod upgrade_from_v3;
 
 use std::borrow::Cow;
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, HashSet};
 use std::path::Path;
 pub use to_runtime_configuration::make_runtime_configuration;
 pub use upgrade_from_v3::upgrade_from_v3;
@@ -74,6 +74,36 @@ impl ParsedConfiguration {
     }
 }
 
+fn get_type_ndc_name(r#type: &metadata::Type) -> &str {
+    match r#type {
+        metadata::Type::CompositeType(ct) => ct.0.as_str(),
+        metadata::Type::ScalarType(t) => t.0.as_str(),
+        metadata::Type::ArrayType(arr_ty) => get_type_ndc_name(arr_ty),
+    }
+}
+
+/// In order to ensure that our schema ends up including exactly the relevant types we need to
+/// inform the introspection query of the types that may appear in native queries, such that they
+/// may be included.
+fn native_query_field_types(native_queries: &metadata::NativeQueries) -> Vec<String> {
+    let mut result: HashSet<&str> = HashSet::new();
+
+    for native_query in native_queries.0.values() {
+        for argument in native_query.arguments.values() {
+            result.insert(get_type_ndc_name(&argument.r#type));
+        }
+
+        for column in native_query.columns.values() {
+            result.insert(get_type_ndc_name(&column.r#type));
+        }
+    }
+
+    result
+        .iter()
+        .map(|t| t.to_string())
+        .collect::<Vec<String>>()
+}
+
 /// Construct the NDC metadata configuration by introspecting the database.
 pub async fn introspect(
     args: ParsedConfiguration,
@@ -106,7 +136,8 @@ pub async fn introspect(
                 .introspection_options
                 .introspect_prefix_function_comparison_operators,
         )
-        .bind(serde_json::to_value(base_type_representations())?);
+        .bind(serde_json::to_value(base_type_representations())?)
+        .bind(native_query_field_types(&args.metadata.native_queries));
 
     let row = connection
         .fetch_one(query)
@@ -118,27 +149,6 @@ pub async fn introspect(
         let scalar_types: metadata::ScalarTypes = serde_json::from_value(row.get(1))?;
         let composite_types: metadata::CompositeTypes = serde_json::from_value(row.get(2))?;
 
-        // TODO!
-        // We need to include `in` as a comparison operator in the schema, and since it is syntax, it is not introspectable.
-        // Instead, we will check if the scalar type defines an equals operator and if yes, we will insert the `_in` operator
-        // as well.
-        // for (scalar_type, operators) in &mut comparison_operators {
-        //     if operators
-        //         .values()
-        //         .any(|op| op.operator_kind == metadata::OperatorKind::Equal)
-        //     {
-        //         operators.insert(
-        //             "_in".to_string(),
-        //             metadata::ComparisonOperator {
-        //                 operator_name: "IN".to_string(),
-        //                 operator_kind: metadata::OperatorKind::In,
-        //                 argument_type: scalar_type.clone(),
-        //                 is_infix: true,
-        //             },
-        //         );
-        //     }
-        // }
-
         // We need to specify the concrete return type explicitly so that rustc knows that it can
         // be sent across an async boundary.
         // (last verified with rustc 1.72.1)
@@ -147,17 +157,6 @@ pub async fn introspect(
     .instrument(info_span!("Decode introspection result"))
     .await?;
 
-    // TODO: Only have occurring types. Maybe do this in SQL instead?
-    // let (scalar_types, composite_types) = transitively_occurring_types(
-    //     occurring_scalar_types(
-    //         &tables,
-    //         &args.metadata.native_queries,
-    //         &args.metadata.aggregate_functions,
-    //     ),
-    //     &occurring_composite_types(&tables, &args.metadata.native_queries),
-    //     composite_types,
-    // );
-    //
     Ok(ParsedConfiguration {
         version: Version::This,
         schema: args.schema,
@@ -218,189 +217,6 @@ fn base_type_representations() -> BTreeMap<String, database::TypeRepresentation>
     ]
     .into()
 }
-
-// /// Collect all the composite types that can occur in the metadata.
-// pub fn occurring_composite_types(
-//     tables: &metadata::TablesInfo,
-//     native_queries: &metadata::NativeQueries,
-// ) -> BTreeSet<String> {
-//     let tables_column_types = tables
-//         .0
-//         .values()
-//         .flat_map(|v| v.columns.values().map(|c| &c.r#type));
-//     let native_queries_column_types = native_queries
-//         .0
-//         .values()
-//         .flat_map(|v| v.columns.values().map(|c| &c.r#type));
-//     let native_queries_arguments_types = native_queries
-//         .0
-//         .values()
-//         .flat_map(|v| v.arguments.values().map(|c| &c.r#type));
-//
-//     tables_column_types
-//         .chain(native_queries_column_types)
-//         .chain(native_queries_arguments_types)
-//         .filter_map(|t| match t {
-//             metadata::Type::CompositeType(ref t) => Some(t.clone()),
-//             metadata::Type::ArrayType(t) => match **t {
-//                 metadata::Type::CompositeType(ref t) => Some(t.clone()),
-//                 metadata::Type::ArrayType(_) | metadata::Type::ScalarType(_) => None,
-//             },
-//             metadata::Type::ScalarType(_) => None,
-//         })
-//         .collect::<BTreeSet<String>>()
-// }
-
-// Since array types and composite types may refer to other types we have to transitively discover
-// the full set of types that are relevant to the schema.
-// pub fn transitively_occurring_types(
-//     mut occurring_scalar_types: BTreeSet<metadata::ScalarType>,
-//     occurring_type_names: &BTreeSet<String>,
-//     mut composite_types: metadata::CompositeTypes,
-// ) -> (BTreeSet<metadata::ScalarType>, metadata::CompositeTypes) {
-//     let mut discovered_type_names = occurring_type_names.clone();
-//
-//     for t in occurring_type_names {
-//         match composite_types.0.get(t) {
-//             None => (),
-//             Some(ct) => {
-//                 for f in ct.fields.values() {
-//                     match &f.r#type {
-//                         metadata::Type::CompositeType(ct2) => {
-//                             discovered_type_names.insert(ct2.to_string());
-//                         }
-//                         metadata::Type::ScalarType(t) => {
-//                             occurring_scalar_types.insert(t.clone());
-//                         }
-//                         metadata::Type::ArrayType(arr_ty) => match **arr_ty {
-//                             metadata::Type::CompositeType(ref ct2) => {
-//                                 discovered_type_names.insert(ct2.to_string());
-//                             }
-//                             metadata::Type::ScalarType(ref t) => {
-//                                 occurring_scalar_types.insert(t.clone());
-//                             }
-//                             metadata::Type::ArrayType(_) => {
-//                                 // This case is impossible, because we do not support nested arrays
-//                             }
-//                         },
-//                     }
-//                 }
-//             }
-//         }
-//     }
-//
-//     // Since 'discovered_type_names' only grows monotonically starting from 'occurring_type_names'
-//     // we just have to compare the number of elements to know if new types have been discovered.
-//     if discovered_type_names.len() == occurring_type_names.len() {
-//         // Iterating over occurring types discovered no new types
-//         composite_types
-//             .0
-//             .retain(|t, _| occurring_type_names.contains(t));
-//         (occurring_scalar_types, composite_types)
-//     } else {
-//         // Iterating over occurring types did discover new types,
-//         // so we keep on going.
-//         transitively_occurring_types(
-//             occurring_scalar_types,
-//             &discovered_type_names,
-//             composite_types,
-//         )
-//     }
-// }
-
-// /// Collect all the scalar types that can occur in the metadata. This is a bit circumstantial. A better
-// /// approach is likely to record scalar type names directly in the metadata via version2.sql.
-// pub fn occurring_scalar_types(
-//     tables: &metadata::TablesInfo,
-//     native_queries: &metadata::NativeQueries,
-//     aggregate_functions: &metadata::AggregateFunctions,
-// ) -> BTreeSet<metadata::ScalarType> {
-//     let tables_column_types = tables
-//         .0
-//         .values()
-//         .flat_map(|v| v.columns.values().map(|c| &c.r#type));
-//     let native_queries_column_types = native_queries
-//         .0
-//         .values()
-//         .flat_map(|v| v.columns.values().map(|c| &c.r#type));
-//     let native_queries_arguments_types = native_queries
-//         .0
-//         .values()
-//         .flat_map(|v| v.arguments.values().map(|c| &c.r#type));
-//     let aggregate_functions_result_types = aggregate_functions
-//         .0
-//         .values()
-//         .flat_map(|x| x.values().map(|agg_fn| agg_fn.return_type.clone()));
-//
-//     tables_column_types
-//         .chain(native_queries_column_types)
-//         .chain(native_queries_arguments_types)
-//         .filter_map(|t| match t {
-//             metadata::Type::ScalarType(ref t) => Some(t.clone()), // only keep scalar types
-//             metadata::Type::ArrayType(_) | metadata::Type::CompositeType(_) => None,
-//         })
-//         .chain(aggregate_functions_result_types)
-//         .collect::<BTreeSet<metadata::ScalarType>>()
-// }
-
-// /// Filter predicate for comparison operators. Preserves only comparison operators that are
-// /// relevant to any of the given scalar types.
-// ///
-// /// This function is public to enable use in later versions that retain the same metadata types.
-// fn filter_comparison_operators(
-//     scalar_types: &BTreeSet<metadata::ScalarType>,
-//     comparison_operators: metadata::ComparisonOperators,
-// ) -> metadata::ComparisonOperators {
-//     metadata::ComparisonOperators(
-//         comparison_operators
-//             .0
-//             .into_iter()
-//             .filter(|(typ, _)| scalar_types.contains(typ))
-//             .map(|(typ, ops)| {
-//                 (
-//                     typ,
-//                     ops.into_iter()
-//                         .filter(|(_, op)| scalar_types.contains(&op.argument_type))
-//                         .collect(),
-//                 )
-//             })
-//             .collect(),
-//     )
-// }
-
-// /// Filter predicate for aggregation functions. Preserves only aggregation functions that are
-// /// relevant to any of the given scalar types.
-// ///
-// /// This function is public to enable use in later versions that retain the same metadata types.
-// fn filter_aggregate_functions(
-//     scalar_types: &BTreeSet<metadata::ScalarType>,
-//     aggregate_functions: metadata::AggregateFunctions,
-// ) -> metadata::AggregateFunctions {
-//     metadata::AggregateFunctions(
-//         aggregate_functions
-//             .0
-//             .into_iter()
-//             .filter(|(typ, _)| scalar_types.contains(typ))
-//             .collect(),
-//     )
-// }
-
-// /// Filter predicate for type representations. Preserves only type representations that are
-// /// relevant to any of the given scalar types.
-// ///
-// /// This function is public to enable use in later versions that retain the same metadata types.
-// fn filter_type_representations(
-//     scalar_types: &BTreeSet<metadata::ScalarType>,
-//     type_representations: metadata::TypeRepresentations,
-// ) -> metadata::TypeRepresentations {
-//     metadata::TypeRepresentations(
-//         type_representations
-//             .0
-//             .into_iter()
-//             .filter(|(typ, _)| scalar_types.contains(typ))
-//             .collect(),
-//     )
-// }
 
 pub async fn parse_configuration(
     configuration_dir: impl AsRef<Path>,
