71c4e251ab7e5bf16c7bae00552f66d09070a1bf
Gil Mizrahi <gil@hasura.io>
2024-05-27 12:53:26 +0300

    use generate_series

--- crates/query-engine/translation/src/translation/mutation/experimental/delete.rs
+++ crates/query-engine/translation/src/translation/mutation/experimental/delete.rs
@@ -173,15 +173,13 @@ pub fn translate_delete(
                     from,
                     where_: sql::ast::Where(where_),
                     // RETURNING *, true
-                    returning: sql::ast::Returning::Returning(
-                        sql::ast::SelectList::SelectListComposite(
-                            Box::new(sql::ast::SelectList::SelectStar),
-                            Box::new(sql::ast::SelectList::SelectList(vec![(
-                                check_constraint_alias.clone(),
-                                check_constraint_value,
-                            )])),
-                        ),
-                    ),
+                    returning: sql::ast::Returning(sql::ast::SelectList::SelectListComposite(
+                        Box::new(sql::ast::SelectList::SelectStar),
+                        Box::new(sql::ast::SelectList::SelectList(vec![(
+                            check_constraint_alias.clone(),
+                            check_constraint_value,
+                        )])),
+                    )),
                 },
                 check_constraint_alias,
             ))
