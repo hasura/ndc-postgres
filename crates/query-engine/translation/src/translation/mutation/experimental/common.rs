afb1c2a52c27963b3da022c7f85adc1e79abdc70
Gil Mizrahi <gil@hasura.io>
2024-06-19 17:24:20 +0300

    Refactor: use NonEmpty and unify fetching of key columns (#502)
    ### What
    
    Quick refactor as a followup to
    https://github.com/hasura/ndc-postgres/pull/500#discussion_r1645749480:
    
    1. We use NonEmpty key columns instead of a simple Vec and trace a
    warning when there are no keys.
    2. We move the common code of key_columns fetching into the `common`
    module and use it for both delete and update.

--- crates/query-engine/translation/src/translation/mutation/experimental/common.rs
+++ crates/query-engine/translation/src/translation/mutation/experimental/common.rs
@@ -1,5 +1,8 @@
 //! Some common helper functions.
 
+use crate::translation::error::Warning;
+use nonempty::NonEmpty;
+use query_engine_metadata::metadata;
 use std::collections::BTreeSet;
 
 /// Create a description string for keys. For example:
@@ -32,4 +35,62 @@ pub fn description_keys(keys: &BTreeSet<String>) -> String {
     }
 
     string
+}
+
+/// Fetch the key columns of a constraint and calculate the constraint name.
+pub fn get_unique_constraint_name_and_key_columns(
+    mutation_type: &str,
+    collection_name: &str,
+    db_constraint_name: &str,
+    table_info: &metadata::database::TableInfo,
+    keys: &metadata::UniquenessConstraint,
+) -> Option<(String, NonEmpty<metadata::database::ColumnInfo>)> {
+    let mut key_columns: Vec<metadata::database::ColumnInfo> = vec![];
+    let mut constraint_name = String::new();
+
+    for (index, key) in keys.0.iter().enumerate() {
+        // We don't expect this to happen because the metadata generated should be consistent,
+        // but if it does, we skip generating these procedure rather than not start at all.
+        let key_column = {
+            if let Some(key_column) = table_info.columns.get(key) {
+                key_column
+            } else {
+                let warning = Warning::GeneratingMutationSkippedBecauseColumnNotFoundInCollection {
+                    mutation_type: mutation_type.to_string(),
+                    collection: collection_name.to_string(),
+                    column: key.clone(),
+                    db_constraint_name: db_constraint_name.to_string(),
+                };
+                tracing::warn!(
+                    info = ?warning,
+                    warning = format!("{warning}"),
+                );
+                None?
+            }
+        };
+
+        key_columns.push(key_column.clone());
+
+        constraint_name.push_str(key);
+        if index + 1 < keys.0.len() {
+            constraint_name.push_str("_and_");
+        }
+    }
+    // There should be at least one key column.
+    let key_columns = if let Some(key_columns) = NonEmpty::from_vec(key_columns) {
+        key_columns
+    } else {
+        let warning = Warning::GeneratingMutationSkippedBecauseNoColumnsInConstraint {
+            mutation_type: mutation_type.to_string(),
+            collection: collection_name.to_string(),
+            db_constraint_name: db_constraint_name.to_string(),
+        };
+        tracing::warn!(
+            info = ?warning,
+            warning = format!("{warning}"),
+        );
+        None?
+    };
+
+    Some((constraint_name, key_columns))
 }
