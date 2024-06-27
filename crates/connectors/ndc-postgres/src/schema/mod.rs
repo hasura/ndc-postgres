86bf6ead402e2b9bc9ad5c23d18e1938dafc8019
Gil Mizrahi <gil@hasura.io>
2024-06-27 15:44:17 +0300

    split ndc-spec schema generation

---
+++ crates/connectors/ndc-postgres/src/schema/mod.rs
@@ -0,0 +1,400 @@
+//! Implement the `/schema` endpoint to return the connector's schema.
+//! See the Hasura
+//! [Native Data Connector Specification](https://hasura.github.io/ndc-spec/specification/schema/index.html)
+//! for further details.
+
+mod helpers;
+mod mutation_experimental;
+mod mutation_v1;
+mod mutation_v2;
+
+use std::collections::BTreeMap;
+
+use ndc_sdk::connector;
+use ndc_sdk::models;
+
+use helpers::*;
+use ndc_postgres_configuration as configuration;
+use query_engine_metadata::metadata;
+use query_engine_translation::translation::helpers::Env;
+use query_engine_translation::translation::mutation;
+
+/// Get the connector's schema.
+///
+/// This function implements the [schema endpoint](https://hasura.github.io/ndc-spec/specification/schema/index.html)
+/// from the NDC specification.
+pub fn get_schema(
+    config: &configuration::Configuration,
+) -> Result<models::SchemaResponse, connector::SchemaError> {
+    let metadata = &config.metadata;
+    let mut scalar_types: BTreeMap<String, models::ScalarType> = metadata
+        .scalar_types
+        .0
+        .iter()
+        .map(|(scalar_type_name, scalar_type_info)| {
+            let result = models::ScalarType {
+                representation: scalar_type_info
+                    .type_representation
+                    .as_ref()
+                    .map(map_type_representation),
+                aggregate_functions: scalar_type_info
+                    .aggregate_functions
+                    .iter()
+                    .map(|(function_name, function_definition)| {
+                        (
+                            function_name.clone(),
+                            models::AggregateFunctionDefinition {
+                                result_type: models::Type::Nullable {
+                                    // It turns out that all aggregates defined for postgres
+                                    // (_except_ `COUNT`) will return `NULL` for an empty row set.
+                                    // Thus, we mark all aggregates as having a nullable return
+                                    // type.
+                                    underlying_type: Box::new(models::Type::Named {
+                                        name: function_definition.return_type.0.clone(),
+                                    }),
+                                },
+                            },
+                        )
+                    })
+                    .collect(),
+                comparison_operators: scalar_type_info
+                    .comparison_operators
+                    .iter()
+                    .map(|(op_name, op_def)| {
+                        (
+                            op_name.clone(),
+                            match op_def.operator_kind {
+                                metadata::OperatorKind::Equal => {
+                                    models::ComparisonOperatorDefinition::Equal
+                                }
+                                metadata::OperatorKind::In => {
+                                    models::ComparisonOperatorDefinition::In
+                                }
+                                metadata::OperatorKind::Custom => {
+                                    models::ComparisonOperatorDefinition::Custom {
+                                        argument_type: models::Type::Named {
+                                            name: op_def.argument_type.0.clone(),
+                                        },
+                                    }
+                                }
+                            },
+                        )
+                    })
+                    .collect(),
+            };
+            (scalar_type_name.0.clone(), result)
+        })
+        .collect();
+
+    let collections_by_identifier: BTreeMap<(&str, &str), &str> = metadata
+        .tables
+        .0
+        .iter()
+        .map(|(collection_name, table)| {
+            (
+                (table.schema_name.as_ref(), table.table_name.as_ref()),
+                collection_name.as_ref(),
+            )
+        })
+        .collect();
+
+    let tables: Vec<models::CollectionInfo> = metadata
+        .tables
+        .0
+        .iter()
+        .map(|(collection_name, table)| models::CollectionInfo {
+            name: collection_name.clone(),
+            description: table.description.clone(),
+            arguments: BTreeMap::new(),
+            collection_type: collection_name.clone(),
+            uniqueness_constraints: table
+                .uniqueness_constraints
+                .0
+                .iter()
+                .map(
+                    |(constraint_name, metadata::UniquenessConstraint(constraint_columns))| {
+                        (
+                            constraint_name.clone(),
+                            models::UniquenessConstraint {
+                                unique_columns: constraint_columns.iter().cloned().collect(),
+                            },
+                        )
+                    },
+                )
+                .collect(),
+            foreign_keys: table
+                .foreign_relations
+                .0
+                .iter()
+                .map(
+                    |(
+                        constraint_name,
+                        metadata::ForeignRelation {
+                            foreign_schema,
+                            foreign_table,
+                            column_mapping,
+                        },
+                    )| {
+                        (
+                            constraint_name.clone(),
+                            models::ForeignKeyConstraint {
+                                foreign_collection: (*collections_by_identifier
+                                    .get(&(
+                                        // the foreign schema used to be implied, so if it is not
+                                        // provided, we need to default back to the originating
+                                        // table's schema
+                                        foreign_schema.as_ref().unwrap_or(&table.schema_name),
+                                        foreign_table,
+                                    ))
+                                    .unwrap_or_else(|| {
+                                        panic!(
+                                            "Unknown foreign table: {foreign_schema:?}.{foreign_table:?}"
+                                        )
+                                    }))
+                                .to_string(),
+                                column_mapping: column_mapping.clone(),
+                            },
+                        )
+                    },
+                )
+                .collect(),
+        })
+        .collect();
+
+    let native_queries: Vec<models::CollectionInfo> = metadata
+        .native_queries
+        .0
+        .iter()
+        .filter(|(_, info)| !info.is_procedure)
+        .map(|(name, info)| models::CollectionInfo {
+            name: name.clone(),
+            description: info.description.clone(),
+            arguments: info
+                .arguments
+                .iter()
+                .map(|(name, readonly_column_info)| {
+                    (
+                        name.clone(),
+                        models::ArgumentInfo {
+                            description: readonly_column_info.description.clone(),
+                            argument_type: readonly_column_to_type(readonly_column_info),
+                        },
+                    )
+                })
+                .collect(),
+            collection_type: name.clone(),
+            uniqueness_constraints: BTreeMap::new(),
+            foreign_keys: BTreeMap::new(),
+        })
+        .collect();
+
+    let mut collections = tables;
+    collections.extend(native_queries);
+
+    let table_types = metadata
+        .tables
+        .0
+        .iter()
+        .map(|(collection_name, table)| {
+            let object_type = models::ObjectType {
+                description: table.description.clone(),
+                fields: table
+                    .columns
+                    .iter()
+                    .map(|(column_name, column_info)| {
+                        (
+                            column_name.clone(),
+                            models::ObjectField {
+                                description: column_info.description.clone(),
+                                r#type: column_to_type(column_info),
+                                arguments: BTreeMap::new(),
+                            },
+                        )
+                    })
+                    .collect(),
+            };
+            (collection_name.clone(), object_type)
+        })
+        .collect::<BTreeMap<_, _>>();
+
+    let native_queries_types = metadata
+        .native_queries
+        .0
+        .iter()
+        .map(|(nq_name, nq_info)| {
+            let object_type = models::ObjectType {
+                description: nq_info.description.clone(),
+                fields: nq_info
+                    .columns
+                    .iter()
+                    .map(|(column_name, column_info)| {
+                        (
+                            column_name.clone(),
+                            models::ObjectField {
+                                description: column_info.description.clone(),
+                                r#type: readonly_column_to_type(column_info),
+                                arguments: BTreeMap::new(),
+                            },
+                        )
+                    })
+                    .collect(),
+            };
+            (nq_name.clone(), object_type)
+        })
+        .collect::<BTreeMap<_, _>>();
+
+    let composite_types = metadata
+        .composite_types
+        .0
+        .iter()
+        .map(|(ctype_name, ctype_info)| {
+            let object_type = models::ObjectType {
+                description: ctype_info.description.clone(),
+                fields: ctype_info
+                    .fields
+                    .iter()
+                    .map(|(field_name, field_info)| {
+                        (
+                            field_name.clone(),
+                            models::ObjectField {
+                                description: field_info.description.clone(),
+                                r#type: type_to_type(&field_info.r#type),
+                                arguments: BTreeMap::new(),
+                            },
+                        )
+                    })
+                    .collect(),
+            };
+            (ctype_name.0.clone(), object_type)
+        })
+        .collect::<BTreeMap<_, _>>();
+
+    let mut object_types = table_types;
+    object_types.extend(native_queries_types);
+    object_types.extend(composite_types);
+
+    let mut procedures: Vec<models::ProcedureInfo> = metadata
+        .native_queries
+        .0
+        .iter()
+        .filter(|(_, nq_info)| nq_info.is_procedure)
+        .map(|(nq_name, nq_info)| {
+            make_procedure_type(
+                nq_name.clone(),
+                nq_info.description.clone(),
+                nq_info
+                    .arguments
+                    .iter()
+                    .map(|(column_name, column_info)| {
+                        (
+                            column_name.clone(),
+                            models::ArgumentInfo {
+                                description: column_info.description.clone(),
+                                argument_type: readonly_column_to_type(column_info),
+                            },
+                        )
+                    })
+                    .collect(),
+                models::Type::Named {
+                    name: nq_name.clone(),
+                },
+                &mut object_types,
+                &mut scalar_types,
+            )
+        })
+        .collect();
+
+    let mut more_object_types = BTreeMap::new();
+    let env = Env::new(metadata, BTreeMap::new(), config.mutations_version, None);
+    let generated_procedures: Vec<models::ProcedureInfo> =
+        query_engine_translation::translation::mutation::generate::generate(&env)
+            .iter()
+            .map(|(name, mutation)| {
+                mutation_to_procedure(name, mutation, &mut more_object_types, &mut scalar_types)
+            })
+            .collect();
+
+    procedures.extend(generated_procedures);
+    object_types.extend(more_object_types);
+
+    Ok(models::SchemaResponse {
+        collections,
+        procedures,
+        functions: vec![],
+        object_types,
+        scalar_types,
+    })
+}
+
+/// Turn our different `Mutation` items into `ProcedureInfo`s to be output in the schema
+fn mutation_to_procedure(
+    name: &String,
+    mutation: &mutation::generate::Mutation,
+    object_types: &mut BTreeMap<String, models::ObjectType>,
+    scalar_types: &mut BTreeMap<String, models::ScalarType>,
+) -> models::ProcedureInfo {
+    match mutation {
+        // v1
+        mutation::generate::Mutation::V1(mutation::v1::Mutation::DeleteMutation(delete)) => {
+            mutation_v1::delete_to_procedure(name, delete, object_types, scalar_types)
+        }
+        mutation::generate::Mutation::V1(mutation::v1::Mutation::InsertMutation(insert)) => {
+            mutation_v1::insert_to_procedure(name, insert, object_types, scalar_types)
+        }
+        // v2
+        mutation::generate::Mutation::V2(mutation::v2::Mutation::DeleteMutation(delete)) => {
+            mutation_v2::delete_to_procedure(name, delete, object_types, scalar_types)
+        }
+        mutation::generate::Mutation::V2(mutation::v2::Mutation::InsertMutation(insert)) => {
+            mutation_v2::insert_to_procedure(name, insert, object_types, scalar_types)
+        }
+        mutation::generate::Mutation::V2(mutation::v2::Mutation::UpdateMutation(update)) => {
+            mutation_v2::update_to_procedure(name, update, object_types, scalar_types)
+        }
+        // experimental
+        mutation::generate::Mutation::Experimental(
+            mutation::experimental::Mutation::DeleteMutation(delete),
+        ) => mutation_experimental::delete_to_procedure(name, delete, object_types, scalar_types),
+        mutation::generate::Mutation::Experimental(
+            mutation::experimental::Mutation::InsertMutation(insert),
+        ) => mutation_experimental::insert_to_procedure(name, insert, object_types, scalar_types),
+        mutation::generate::Mutation::Experimental(
+            mutation::experimental::Mutation::UpdateMutation(update),
+        ) => mutation_experimental::update_to_procedure(name, update, object_types, scalar_types),
+    }
+}
+
+/// Map our local type representation to ndc-spec type representation.
+#[allow(clippy::match_same_arms)] // merging arms would require changing the order, making this harder to understand
+fn map_type_representation(
+    type_representation: &metadata::TypeRepresentation,
+) -> models::TypeRepresentation {
+    match type_representation {
+        metadata::TypeRepresentation::Boolean => models::TypeRepresentation::Boolean,
+        metadata::TypeRepresentation::String => models::TypeRepresentation::String,
+        metadata::TypeRepresentation::Float32 => models::TypeRepresentation::Float32,
+        metadata::TypeRepresentation::Float64 => models::TypeRepresentation::Float64,
+        metadata::TypeRepresentation::Int16 => models::TypeRepresentation::Int16,
+        metadata::TypeRepresentation::Int32 => models::TypeRepresentation::Int32,
+        // Int64 returns a number.
+        metadata::TypeRepresentation::Int64 => models::TypeRepresentation::JSON,
+        // Int64AsString returns a string.
+        metadata::TypeRepresentation::Int64AsString => models::TypeRepresentation::Int64,
+        // BigDecimal returns a number.
+        metadata::TypeRepresentation::BigDecimal => models::TypeRepresentation::JSON,
+        // BigDecimalAsString returns a string.
+        metadata::TypeRepresentation::BigDecimalAsString => models::TypeRepresentation::BigDecimal,
+        metadata::TypeRepresentation::Timestamp => models::TypeRepresentation::Timestamp,
+        metadata::TypeRepresentation::Timestamptz => models::TypeRepresentation::TimestampTZ,
+        metadata::TypeRepresentation::Time => models::TypeRepresentation::String,
+        metadata::TypeRepresentation::Timetz => models::TypeRepresentation::String,
+        metadata::TypeRepresentation::Date => models::TypeRepresentation::Date,
+        metadata::TypeRepresentation::Geometry => models::TypeRepresentation::Geometry,
+        metadata::TypeRepresentation::Geography => models::TypeRepresentation::Geography,
+        metadata::TypeRepresentation::UUID => models::TypeRepresentation::UUID,
+        metadata::TypeRepresentation::Json => models::TypeRepresentation::JSON,
+        metadata::TypeRepresentation::Enum(variants) => models::TypeRepresentation::Enum {
+            one_of: variants.clone(),
+        },
+    }
+}
---
+++ crates/connectors/ndc-postgres/src/schema/mutation_v1.rs
@@ -0,0 +1,79 @@
+//! Exposing v1 auto-generated mutations in the ndc-spec Schema.
+
+use std::collections::BTreeMap;
+
+use ndc_sdk::models;
+
+use query_engine_translation::translation::mutation;
+
+use super::helpers::*;
+
+/// given a v1 `DeleteMutation`, turn it into a `ProcedureInfo` to be output in the schema
+pub fn delete_to_procedure(
+    name: &String,
+    delete: &mutation::v1::delete::DeleteMutation,
+    object_types: &mut BTreeMap<String, models::ObjectType>,
+    scalar_types: &mut BTreeMap<String, models::ScalarType>,
+) -> models::ProcedureInfo {
+    match delete {
+        mutation::v1::delete::DeleteMutation::DeleteByKey {
+            by_column,
+            description,
+            collection_name,
+            ..
+        } => {
+            let mut arguments = BTreeMap::new();
+
+            arguments.insert(
+                by_column.name.clone(),
+                models::ArgumentInfo {
+                    argument_type: column_to_type(by_column),
+                    description: by_column.description.clone(),
+                },
+            );
+
+            make_procedure_type(
+                name.to_string(),
+                Some(description.to_string()),
+                arguments,
+                models::Type::Named {
+                    name: collection_name.to_string(),
+                },
+                object_types,
+                scalar_types,
+            )
+        }
+    }
+}
+
+/// Given a v1 `InsertMutation`, turn it into a `ProcedureInfo` to be output in the schema.
+pub fn insert_to_procedure(
+    name: &String,
+    insert: &mutation::v1::insert::InsertMutation,
+    object_types: &mut BTreeMap<String, models::ObjectType>,
+    scalar_types: &mut BTreeMap<String, models::ScalarType>,
+) -> models::ProcedureInfo {
+    let mut arguments = BTreeMap::new();
+    let object_type = make_object_type(&insert.columns);
+    let object_name = format!("{name}_object");
+    object_types.insert(object_name.clone(), object_type);
+
+    arguments.insert(
+        "_object".to_string(),
+        models::ArgumentInfo {
+            argument_type: models::Type::Named { name: object_name },
+            description: None,
+        },
+    );
+
+    make_procedure_type(
+        name.to_string(),
+        Some(insert.description.to_string()),
+        arguments,
+        models::Type::Named {
+            name: insert.collection_name.to_string(),
+        },
+        object_types,
+        scalar_types,
+    )
+}
