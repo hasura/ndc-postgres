57d37ba2e9bdc8bda8fa25167624a3b3fcabe383
Gil Mizrahi <gil@hasura.io>
2024-07-31 09:36:08 +0300

    new-version.sh script for new version PRs (#556)
    ### What
    
    We want to automate some of the release process for ndc-postgres by
    introducing a new `scripts/new-version.sh` script that updates the cargo
    package version and updates the changelog.
    
    Run with: `scripts/new-version.sh 1.0.2`, for example.
    
    ### How
    
    1. A bash script to run everything
    2. A sed script to change the package version
    3. A Haskell script to manipulate the changelog to:
       - Change the unreleased header to the version
       - Insert a new unreleased template
       - Fix the references at the bottom of the changelog

---
+++ scripts/new-version.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Update cargo packages version and changelog entries for a new version.
+
+set -e -u -o pipefail
+
+if [ "$#" != 1 ];
+then
+  echo "Usage: $0 <new-version>"
+  exit 1
+fi
+
+NEW_VERSION="$1"
+DATE_TODAY=$(date +"%Y-%m-%d")
+
+echo "Updating version to v$NEW_VERSION"
+
+sed -i "s/package.version = .*/package.version = \"${NEW_VERSION}\"/" Cargo.toml
+
+just test
+
+runghc scripts/changelog.hs "changelog.md" "${DATE_TODAY}" "${NEW_VERSION}"
