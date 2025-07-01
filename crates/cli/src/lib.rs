45daadc0a541fb17771f0fd826a9cdb8b1b4ecf2
Benoit Ranque <benoit@hasura.io>
2025-07-01 16:22:20 -0700

    Update CI to automate release process (#784)
    Automate CI
    
    This PR adds workflows to automate CI.
    Once merged, we can trigger the "create release" workflow to create a
    release PR. When that is merged, the new version is tagged and released
    automatically.
    
    We also inline the cli plugin definitions. To do this, we deprecate
    using the postgres cli to generate the metadata for ci, and instead we
    use templates in ci.

--- crates/cli/src/lib.rs
+++ crates/cli/src/lib.rs
@@ -3,13 +3,11 @@
 //! The CLI can do a few things. This provides a central point where those things are routed and
 //! then done, making it easier to test this crate deterministically.
 
-mod metadata;
 mod native_operations;
 
 use std::path::PathBuf;
 
 use clap::Subcommand;
-use metadata::NativeToolchainDefinition;
 use tokio::fs;
 
 use ndc_postgres_configuration as configuration;
@@ -28,11 +26,7 @@ pub struct Context<Env: Environment> {
 #[derive(Debug, Clone, Subcommand)]
 pub enum Command {
     /// Initialize a configuration in the current (empty) directory.
-    Initialize {
-        #[arg(long)]
-        /// Whether to create the hasura connector metadata.
-        with_metadata: bool,
-    },
+    Initialize,
     /// Update the configuration by introspecting the database, using the configuration options.
     Update,
     /// Upgrade the configuration to the latest version. This does not involve the database.
@@ -56,7 +50,7 @@ pub enum Error {
 /// Run a command in a given directory.
 pub async fn run(command: Command, context: Context<impl Environment>) -> anyhow::Result<()> {
     match command {
-        Command::Initialize { with_metadata } => initialize(with_metadata, context).await?,
+        Command::Initialize => initialize(context).await?,
         Command::Update => update(context).await?,
         Command::Upgrade { dir_from, dir_to } => upgrade(dir_from, dir_to).await?,
         Command::NativeOperation(cmd) => native_operations::run(cmd, context).await?,
@@ -72,7 +66,7 @@ pub async fn run(command: Command, context: Context<impl Environment>) -> anyhow
 ///
 /// Optionally, this can also create the connector metadata, which is used by the Hasura CLI to
 /// automatically work with this CLI as a plugin.
-async fn initialize(with_metadata: bool, context: Context<impl Environment>) -> anyhow::Result<()> {
+async fn initialize(context: Context<impl Environment>) -> anyhow::Result<()> {
     // refuse to initialize the directory unless it is empty
     let mut items_in_dir = fs::read_dir(&context.context_path).await?;
     if items_in_dir.next_entry().await?.is_some() {
@@ -84,79 +78,6 @@ async fn initialize(with_metadata: bool, context: Context<impl Environment>) -> 
         &context.context_path,
     )
     .await?;
-
-    // if requested, create the metadata
-    if with_metadata {
-        let metadata_dir = context.context_path.join(".hasura-connector");
-        fs::create_dir(&metadata_dir).await?;
-        let metadata_file = metadata_dir.join("connector-metadata.yaml");
-        let metadata = metadata::ConnectorMetadataDefinition {
-            version: metadata::Version::V2,
-            ndc_spec_generation: metadata::NdcSpecGeneration::V0_2,
-            packaging_definition: metadata::PackagingDefinition::PrebuiltDockerImage(
-                metadata::PrebuiltDockerImagePackaging {
-                    docker_image: format!(
-                        "ghcr.io/hasura/ndc-postgres:{}",
-                        context.release_version.unwrap_or("latest")
-                    ),
-                },
-            ),
-            supported_environment_variables: vec![
-				metadata::EnvironmentVariableDefinition {
-					name: "CONNECTION_URI".to_string(),
-					description: "The PostgreSQL connection URI".to_string(),
-					default_value: Some("postgresql://read_only_user:readonlyuser@35.236.11.122:5432/v3-docs-sample-app".to_string()),
-				},
-				metadata::EnvironmentVariableDefinition {
-					name: "CLIENT_CERT".to_string(),
-					description: "The SSL client certificate (Optional)".to_string(),
-					default_value: Some(String::new()),
-				},
-				metadata::EnvironmentVariableDefinition {
-					name: "CLIENT_KEY".to_string(),
-					description: "The SSL client key (Optional)".to_string(),
-					default_value: Some(String::new()),
-				},
-				metadata::EnvironmentVariableDefinition {
-					name: "ROOT_CERT".to_string(),
-					description: "The SSL root certificate (Optional)".to_string(),
-					default_value: Some(String::new()),
-				},
-			],
-            commands: metadata::Commands {
-                update: Some("hasura-ndc-postgres update".to_string()),
-                watch: None,
-            },
-            cli_plugin: Some(metadata::CliPluginDefinition {
-                name: "ndc-postgres".to_string(),
-                version: context.release_version.unwrap_or("latest").to_string(),
-            }),
-            docker_compose_watch: vec![metadata::DockerComposeWatchItem {
-                path: "./".to_string(),
-                target: Some("/etc/connector".to_string()),
-                action: metadata::DockerComposeWatchAction::SyncAndRestart,
-                ignore: vec![],
-            }],
-            native_toolchain_definition: Some(NativeToolchainDefinition {
-                commands: vec![
-                    ("start".to_string(), metadata::CommandDefinition::ShellScript {
-                        bash: "#!/usr/bin/env bash\nset -eu -o pipefail\nHASURA_CONFIGURATION_DIRECTORY=\"$HASURA_PLUGIN_CONNECTOR_CONTEXT_PATH\" \"$HASURA_DDN_NATIVE_CONNECTOR_DIR/ndc-postgres\" serve".to_string(),
-                        powershell: "$ErrorActionPreference = \"Stop\"\n$env:HASURA_CONFIGURATION_DIRECTORY=\"$env:HASURA_PLUGIN_CONNECTOR_CONTEXT_PATH\"; & \"$env:HASURA_DDN_NATIVE_CONNECTOR_DIR\\ndc-postgres.exe\" serve".to_string(),
-                    }),
-                    ("update".to_string(), metadata::CommandDefinition::ShellScript {
-                        bash: "#!/usr/bin/env bash\nset -eu -o pipefail\n\"$HASURA_DDN_NATIVE_CONNECTOR_PLUGIN_DIR/hasura-ndc-postgres\" update".to_string(),
-                        powershell: "$ErrorActionPreference = \"Stop\"\n& \"$env:HASURA_DDN_NATIVE_CONNECTOR_PLUGIN_DIR\\hasura-ndc-postgres.exe\" update".to_string(),
-                    }),
-                    ("watch".to_string(), metadata::CommandDefinition::ShellScript {
-                        bash: "#!/usr/bin/env bash\necho \"Watch is not supported for this connector\"\nexit 1".to_string(),
-                        powershell: "Write-Output \"Watch is not supported for this connector\"\nexit 1".to_string(),
-                    }),
-                ].into_iter().collect(),
-            })
-        };
-
-        fs::write(metadata_file, serde_yaml::to_string(&metadata)?).await?;
-    }
 
     Ok(())
 }
--- crates/cli/tests/initialize_tests.rs
+++ crates/cli/tests/initialize_tests.rs
@@ -15,13 +15,7 @@ async fn test_initialize_directory() -> anyhow::Result<()> {
         environment: configuration::environment::EmptyEnvironment,
         release_version: None,
     };
-    run(
-        Command::Initialize {
-            with_metadata: false,
-        },
-        context,
-    )
-    .await?;
+    run(Command::Initialize, context).await?;
 
     let configuration_schema_file_path = dir.path().join("schema.json");
     assert!(configuration_schema_file_path.exists());
@@ -51,13 +45,7 @@ async fn test_initialize_version_is_unchanged() -> anyhow::Result<()> {
         environment: configuration::environment::EmptyEnvironment,
         release_version: None,
     };
-    run(
-        Command::Initialize {
-            with_metadata: false,
-        },
-        context,
-    )
-    .await?;
+    run(Command::Initialize, context).await?;
 
     let configuration_file_path = dir.path().join("configuration.json");
     assert!(configuration_file_path.exists());
@@ -88,94 +76,13 @@ async fn test_do_not_initialize_when_files_already_exist() -> anyhow::Result<()>
         environment: configuration::environment::EmptyEnvironment,
         release_version: None,
     };
-    match run(
-        Command::Initialize {
-            with_metadata: false,
-        },
-        context,
-    )
-    .await
-    {
+    match run(Command::Initialize, context).await {
         Ok(()) => panic!("Expected the command to fail."),
         Err(error) => match error.downcast::<Error>() {
             Err(input) => panic!("Expected a CLI error, but got {input}"),
             Ok(cli_error) => assert_eq!(cli_error, Error::DirectoryIsNotEmpty),
         },
     }
-
-    Ok(())
-}
-
-#[tokio::test]
-async fn test_initialize_directory_with_metadata() -> anyhow::Result<()> {
-    let dir = tempfile::tempdir()?;
-
-    let context = Context {
-        context_path: dir.path().to_owned(),
-        environment: configuration::environment::EmptyEnvironment,
-        release_version: None,
-    };
-    run(
-        Command::Initialize {
-            with_metadata: true,
-        },
-        context,
-    )
-    .await?;
-
-    let configuration_schema_file_path = dir.path().join("schema.json");
-    assert!(configuration_schema_file_path.exists());
-    common::assert_file_ends_with_newline(&configuration_schema_file_path).await?;
-
-    let configuration_file_path = dir.path().join("configuration.json");
-    assert!(configuration_file_path.exists());
-    common::assert_file_ends_with_newline(&configuration_file_path).await?;
-
-    let metadata_file_path = dir
-        .path()
-        .join(".hasura-connector")
-        .join("connector-metadata.yaml");
-    assert!(metadata_file_path.exists());
-    let contents = fs::read_to_string(metadata_file_path).await?;
-    common::assert_ends_with_newline(&contents);
-    insta::assert_snapshot!(contents);
-
-    Ok(())
-}
-
-#[tokio::test]
-async fn test_initialize_directory_with_metadata_and_release_version() -> anyhow::Result<()> {
-    let dir = tempfile::tempdir()?;
-
-    let context = Context {
-        context_path: dir.path().to_owned(),
-        environment: configuration::environment::EmptyEnvironment,
-        release_version: Some("v1.2.3"),
-    };
-    run(
-        Command::Initialize {
-            with_metadata: true,
-        },
-        context,
-    )
-    .await?;
-
-    let configuration_schema_file_path = dir.path().join("schema.json");
-    assert!(configuration_schema_file_path.exists());
-    common::assert_file_ends_with_newline(&configuration_schema_file_path).await?;
-
-    let configuration_file_path = dir.path().join("configuration.json");
-    assert!(configuration_file_path.exists());
-    common::assert_file_ends_with_newline(&configuration_file_path).await?;
-
-    let metadata_file_path = dir
-        .path()
-        .join(".hasura-connector")
-        .join("connector-metadata.yaml");
-    assert!(metadata_file_path.exists());
-    let contents = fs::read_to_string(metadata_file_path).await?;
-    common::assert_ends_with_newline(&contents);
-    insta::assert_snapshot!(contents);
 
     Ok(())
 }
