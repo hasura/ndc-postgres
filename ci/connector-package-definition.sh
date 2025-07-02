0e4091024cc91edd25f50df4bbb0ca97adc8079a
Benoit Ranque <benoit@hasura.io>
2025-07-02 10:19:23 -0700

    fix extension of cli plugin url: we use gnu! Not musl! (#798)
    Fix two bugs in ci:
    - was missing reference to connector tests in connector packaging
    - url for linux distributions was wrong! We use gnu not musl
    - that bug also affected ability to get sha sum

--- ci/connector-package-definition.sh
+++ ci/connector-package-definition.sh
@@ -24,10 +24,10 @@ chmod +x ./${RELEASE_DIR}/artifacts/ndc-postgres-cli-*
 
 # export env vars for templating
 export RELEASE_VERSION="$RELEASE_VERSION"
-export LINUX_AMD64_SHA256=$(sha256sum ${RELEASE_DIR}/artifacts/ndc-postgres-cli-x86_64-unknown-linux-musl     | cut -f1 -d' ')
+export LINUX_AMD64_SHA256=$(sha256sum ${RELEASE_DIR}/artifacts/ndc-postgres-cli-x86_64-unknown-linux-gnu      | cut -f1 -d' ')
 export MACOS_AMD64_SHA256=$(sha256sum ${RELEASE_DIR}/artifacts/ndc-postgres-cli-x86_64-apple-darwin           | cut -f1 -d' ')
 export WINDOWS_AMD64_SHA256=$(sha256sum ${RELEASE_DIR}/artifacts/ndc-postgres-cli-x86_64-pc-windows-msvc.exe  | cut -f1 -d' ')
-export LINUX_ARM64_SHA256=$(sha256sum ${RELEASE_DIR}/artifacts/ndc-postgres-cli-aarch64-unknown-linux-musl    | cut -f1 -d' ')
+export LINUX_ARM64_SHA256=$(sha256sum ${RELEASE_DIR}/artifacts/ndc-postgres-cli-aarch64-unknown-linux-gnu     | cut -f1 -d' ')
 export MACOS_ARM64_SHA256=$(sha256sum ${RELEASE_DIR}/artifacts/ndc-postgres-cli-aarch64-apple-darwin          | cut -f1 -d' ')
 
 # add the connector metadata from template
